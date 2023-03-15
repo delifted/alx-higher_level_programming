2f40b9e18c0d632dedccfbc31be70da09d1ec73d
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-03-15 15:35:53 +0100

    Corrected Calculator Program

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,21 +3,16 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-#a = sys.argv[1]
-#operator = sys.argv[2]
-#b = sys.argv[3]
-
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 
 a = sys.argv[1]
-operator = sys.argv[2]
+c = sys.argv[2]
 b = sys.argv[3]
 
-operator = ['+', '-', '*', '/']
-#for i in operator:
-if not ('+' in sys.argv[2] or '-' in sys.argv[2] or '*' in sys.argv[2]  or '/' in sys.argv[2]):
+c = ['+', '-', '*', '/']
+if not ('+' in c or '-' in c or '*' in c or '/' in c):
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 
@@ -28,8 +23,7 @@ elif sys.argv[2] == '-':
     print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
 elif sys.argv[2] == '*':
     print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
-else:
+elif sys.argv[2] == '/':
     print("{} / {} = {}".format(a, b, div(int(a), int(b))))
-
-
-
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
