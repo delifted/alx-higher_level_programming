b6c9b03b121f79fa4f679de0db9053b9d63c975a
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-03-18 23:03:40 +0100

    Function that Returns a Key with the Biggest Value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    """
     if not a_dictionary:
         return None
     else:
         return max(a_dictionary.keys())
+    """
+    if not a_dictionary:
+        return None
+    else:
+        highest_key = None
+        for key in a_dictionary:
+            if highest_key is None or key > highest_key:
+                highest_key = key
+        return highest_key
+
