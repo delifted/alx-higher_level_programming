3bf1a74c7ae013ff165b56affd9fa83f86688e0e
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-03-19 12:32:43 +0100

    Roman to Integer Function Converter

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_map={'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     prev_value = None
 
@@ -20,16 +20,18 @@ def roman_to_int(roman_string):
 
     return result
 """
+
+
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_dg = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     prev_value = 0
 
     for i in range(len(roman_string) - 1, -1, -1):
-        value = roman_dict[roman_string[i]]
+        value = rom_dg[roman_string[i]]
         if value < prev_value:
             result -= value
         else:
