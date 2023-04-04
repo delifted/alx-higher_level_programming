731167b6bf7bcf77f33fa006f8ad690a19e36df4
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-04-04 08:51:40 +0100

    Square Class Function

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""A module that defines a square"""
+
+
+class Square:
+    """Defines a Square"""
+
+    def __init__(self, size=0):
+        """Initializing Square Size
+        Args:
+            size: represents the size of the square definition
+        Raises:
+            TypeError: when size is not an integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Function to Retrive Size of Square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("\n")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
+
