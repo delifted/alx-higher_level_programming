f1b3de1750f25a4e8e4406f795dce5884beb3261
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-04-19 19:42:30 +0100

    Function to lookup attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,7 +4,5 @@ def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     # Get the object's dictionary of attributes and methods
     attrs = dir(obj)
-    # Filter out any private or special attributes
-    attrs = [attr for attr in attrs if not attr.startswith('__')]
     # Return the list of attributes
     return attrs
