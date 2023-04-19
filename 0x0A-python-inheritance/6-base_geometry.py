bc418bdb2690b5b27b3936a8613b0395484ecd8a
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-04-19 20:36:52 +0100

    Class with unimplemented area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class defined"""
+
+
+class BaseGeometry:
+    '''Class represents BaseGeometry'''
+
+    def area(self):
+        '''method not implemented yet'''
+        raise Exception("area() is not implemented")
