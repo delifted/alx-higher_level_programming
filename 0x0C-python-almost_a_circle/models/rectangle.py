7154751f1f94a7ccfeeb43da6f23564c755d8063
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-04-26 16:07:46 +0100

    Rectangle Clas

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,28 +2,11 @@
 '''Module'''
 
 
-#from models.base import Base
-class Base:
-    """Represents base of all classes created """
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """ """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from models.base import Base
 
 
 class Rectangle(Base):
     ''' '''
-
-#    Base.__width = width
-#    Base.__height = height
-#    Base.__x = x
-#    Base.__y = y
 
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' '''
