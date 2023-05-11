196eed18688efc78a81e1e1bdb95e1a11f4ded81
Oluwasegun Oyelola <seeguncrown2009@gmail.com>
2023-05-11 07:31:33 +0100

    Rectangle Class Base updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,10 +6,18 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    ''' '''
+    '''Represents a rectangle '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        ''' '''
+        '''Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle
+            x (int): The x coordinate of the Rectangle
+            y (int): The y coordinate of the new Rectangle
+            id (int): The identity of the new Rectangle.
+        '''
         self.width = width
         self.height = height
         self.x = x
