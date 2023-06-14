2939490c13789e700f316f0d59fd502912188efd
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-06-14 17:29:18 +0100

    JS Task 3 - JavaScript is amazing

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
 
-console.log(process.argv[2] ? process.argv[2] : 'No argument');
+const argv = process.argv;
+const hasArguments = argv[2] !== undefined;
+
+if (hasArguments) {
+  console.log(`${argv[2]}`);
+} else {
+  console.log('No argument');
+}
