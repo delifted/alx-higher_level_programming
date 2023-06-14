862ea31342139d158fc7eddde48ff79b64c31a91
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-06-14 19:25:45 +0100

    JS Task 6 - JavaScript is amazing

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-
+/*
 const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < myVar.length; i++) {
   console.log(myVar[i]);
-}
+}*/
+
+
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+myVar.forEach((val, i) => {
+  console.log(`${val}`);
+});
