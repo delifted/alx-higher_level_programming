7166938071590c798f7ef4ad0e960dc8e4080129
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-06-14 15:14:19 +0100

    JS Task 1 - JavaScript is amazing

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+if (process.argv.length < 3) {
+	console.log('No argument')
+	};
+
+else if (process.argv.length >= 3) {
+	console.log('Argument found')
+	};
+
+/*for (let i = 2; i < process.argv.length; i++) {
+	console.log( ${i}: ${argv[i]} );
+};*/
