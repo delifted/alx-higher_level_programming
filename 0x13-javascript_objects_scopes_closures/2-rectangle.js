c3e6dee6b16bce584239db439283c5f356ebd35a
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-06-18 13:11:43 +0100

    Rectangle Constructor Initialized If Args are 0

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+
+	  if (w < 1 || h < 1) {
+		  Rectangle: {}
+	  }
+  }
+};
