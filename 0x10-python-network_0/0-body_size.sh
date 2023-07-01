f5c0f7ffaf0d4f407f1a8db9eb771fa944a21548
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-07-01 09:35:47 +0100

    $commit_message

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
+if [ -z "$1" ]; then echo "Please provide a URL as an argument."; exit 1; fi
+response=$(mktemp); curl -s -o "$response" "$1"; size=$(wc -c < "$response"); echo "$size"; rm "$response"
 
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-	echo "Please provide a URL as an argument."
-	exit 1
-fi
-
-# Send the request and store the response in a temporary file
-response=$(mktemp)
-curl -s -o "$response" "$1"
-
-# Get the size of the response in bytes
-size=$(wc -c < "$response")
-
-# Display the size of the response body
-echo "$size"
