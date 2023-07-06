4d25bd9d58e19838a872a80869d347f6f7de262b
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-07-06 23:37:07 +0100

    Script that fetches URL

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
 
     res = requests.get(url)
-    
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
