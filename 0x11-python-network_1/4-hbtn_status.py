f638ff726e5f3ec446031f4da97e312937d3da50
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-07-06 23:36:10 +0100

    Script that fetches URL

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script fetches url"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    res = requests.get(url)
+    
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
