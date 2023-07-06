385f499989e9d714515b8c7968d291897c5d75b7
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-07-06 23:05:25 +0100

    Script takes a URL and an email and sends a POST request to the passed URL with the email as parameter

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script takes in a URL and an email, sends a POST request"""
+
+import sys
+from urllib import request, parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    #Encode the email parameter
+    params = parse.urlencode({'email': email}).encode('utf-8')
+
+    #Send the POST request and retrieve the response
+    with request.urlopen(url, data=params) as response:
+        body = response.read().decode('utf-8')
+
+    print(body)
