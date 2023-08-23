8d3542b0a83939032e605cd8498dc1fee3b95c59
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-08-23 11:25:04 +0100

    Starwards Movie Character Count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,32 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
+
+/*
+ * #!/usr/bin/node
+
+const request = require('request');
 
 const apiURL = process.argv[2];
 
@@ -13,3 +39,4 @@ request(apiURL, function (error, response, body) {
   );
   console.log(wedgeAntillesMovies.length);
 });
+*/
